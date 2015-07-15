8704e25da72dc1c0256ea5d0e6eb8f2c49ad532b
Jeff Cohen <cohen.jeff@gmail.com>
2015-07-14 20:17:25 -0500

    The Golden 7

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,17 @@
 class MoviesController < ApplicationController
 
+  def update
+    movie = Movie.find_by(:id => params["id"])
+    movie.title = params[:title]
+    movie.year = params[:year]
+    movie.save
+    redirect_to root_url
+  end
+
+  def edit
+    @movie = Movie.find_by(:id => params["id"])
+  end
+
   def new
     logger.debug Movie.count.to_s
   end
--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,15 @@ Rails.application.routes.draw do
   get '/directors' => 'directors#index'
   get '/directors/:id' => 'directors#show'
 
-  get '/movies/new' => 'movies#new'
-  get '/movies/create' => 'movies#create'
-  get '/movies/:id' => 'movies#show'
+  # This is the Golden 7 for Movies
   get '/movies' => 'movies#index'
-  get '/movies/:id/delete' => 'movies#destroy'
-  get '/movies/:id/edit' => 'movies#index'
-  get '/movies/:id/update' => 'movies#index'
+  get '/movies/new' => 'movies#new'
+  post '/movies' => 'movies#create'
 
+  get '/movies/:id' => 'movies#show'
+  get '/movies/:id/edit' => 'movies#edit'
+  patch '/movies/:id' => 'movies#update'
+
+  delete '/movies/:id' => 'movies#destroy'
 
 end
